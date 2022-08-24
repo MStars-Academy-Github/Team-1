9618fb395f5ce7424487942b0fe2f51afc44f321
mstart_lab3_19 <mstart_lab3_19@mstart-lab3-19s-iMac.local>
2022-08-24 13:20:14 +0800

    feat: fixing new user controller and user router json return

--- back-end/controller/UserController.ts
+++ back-end/controller/UserController.ts
@@ -17,7 +17,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     req.body;
   const foundUser = await userServices.findUserByEmail(email);
 
-  if (foundUser) {
+  if (foundUser.length > 0) {
     res.json({
       success: false,
       data: "User already exists",
@@ -34,21 +34,28 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         sex: sex,
         password: encryptedPassword,
       });
-      res.status(200).send("User created successfully");
+      res
+        .status(200)
+        .json({ success: true, data: "User created successfully" });
     } catch (error) {
-      return res.status(500).send(getErrorMessage(error));
+      return res
+        .status(500)
+        .json({ success: false, data: getErrorMessage(error) });
     }
   }
+  next();
 };
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
   try {
     const foundUser = await userServices.login(email, password);
-
-    res.status(200).send(foundUser);
+    res.status(200).json({ success: true, data: foundUser });
   } catch (error) {
-    return res.status(500).send(getErrorMessage(error));
+    return res
+      .status(500)
+      .json({ success: true, data: getErrorMessage(error) });
   }
+  next();
 };
 export default { getUsers, createUser, login };
--- back-end/routes/userRouter.ts
+++ back-end/routes/userRouter.ts
@@ -4,7 +4,6 @@ import UserController from "../controller/UserController";
 const router: Router = express.Router();
 
 router.get("/", UserController.getUsers);
-
 router.post("/register", UserController.createUser);
 router.post("/login", UserController.login);
 
