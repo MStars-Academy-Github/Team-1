f94ca06b12cc6e1ac7b768294999f5733e85ebbb
mstars_lab_04 <mstars_lab_04@mstars-lab-04s-iMac.local>
2022-09-01 15:36:11 +0800

    feat: index added

--- Project-2-Video-Streaming-App/front-end/pages/index.tsx
+++ Project-2-Video-Streaming-App/front-end/pages/index.tsx
@@ -1,25 +1,71 @@
 import type { NextPage } from "next";
+<<<<<<< HEAD:Project-2-Video-Streaming-App/front-end/pages/index.tsx
+=======
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import BasicModal from "./login";
+>>>>>>> dea4729 (feat: index added):VideoStreaming/Project-2/front-end/video-streamin-app/pages/index.tsx
 
 const Home: NextPage = () => {
   return (
     <div>
-      <main>
-        <div>
-          <h1 className="text-xl-3 rounded-md bg-blue-600">
-            My anime player yahoo!
-          </h1>
-        </div>
-      </main>
+      <div>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static" className="nav-bar">
+            <Toolbar>
+              <IconButton
+                className="menu-button"
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1 }}
+                className="head-line"
+              >
+                Enjoy with us
+              </Typography>
+              <Button className="signUp-button" variant="contained">
+                Sign in
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </div>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Chuka
-        </a>
-      </footer>
+      <div>
+        <div className="background-area">
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      {/* login form section head */}
+
+      <BasicModal />
+
+      {/* login form section end */}
     </div>
   );
 };
